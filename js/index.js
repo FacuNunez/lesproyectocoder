db6837f72498911487e422fa62e90d7c7dd397e0
Facundo Nuñez <fnunez1981@gmail.com>
2022-09-22 23:46:37 -0300

    primera pre entrega

--- js/index.js
+++ js/index.js
@@ -1,34 +1,50 @@
+const productos = [
+    { id: 1, nombre: "Remera Vida", precio: 3000 },
+    { id: 2, nombre: "Campera Fleco", precio: 6900 },
+    { id: 3, nombre: "Sweater Lara", precio: 5250 },
+    { id: 4, nombre: "Remeron Animal", precio: 3000 },
+    { id: 5, nombre: "Babucha", precio: 6300 },
+    { id: 6, nombre: "Campera Lucia", precio: 11000 },
+    { id: 7, nombre: "Saco Lila", precio: 5300 },
+    { id: 8, nombre: "Sweater Vilma", precio: 4500 },
+    { id: 9, nombre: "Buzo Rock", precio: 6700 },
+    { id: 10, nombre: "Home Spray x 250ml", precio: 750 },
+    { id: 11, nombre: "Home Spray x 750ml", precio: 1200 },
+    { id: 12, nombre: "Short Dulce", precio: 1900 },
+    { id: 13, nombre: "Top Bote", precio: 2500 },
+    { id: 14, nombre: "Remera Rolling", precio: 3000 },
+];
 
-let total = 0;
-let producto = 0;
-let contador = 0;
-let porcentaje = 20
+const compras = [];
 
-function ingresarValor() {
-    producto = Number(prompt("Ingrese el valor del producto" + (contador + 1)));
-    while (producto > 0) {
-        total = calcularTotal(producto);
-        contador++;
-        producto = Number(prompt("Ingrese el valor del producto" + (contador + 1)));
-    }
-
+let seleccion = prompt("Hola, somos Les Bs.As" + " ," + "empezamos : ");
+if (seleccion == "si") {
+    let mostrar = productos.map(
+        (producto) =>
+        " " + producto.id + "-" + producto.nombre + " $" + producto.precio
+    );
+    alert(mostrar);
+} else if (seleccion == "no") {
+    alert("Gracias, volve pronto! te esperamos!");
 }
 
-function calcularTotal(producto) {
-    return total + producto;
+let eleccion = prompt("Ingresa el nombre producto seleccionado: ");
+let encontrado = productos.find((prenda) => prenda.nombre === eleccion);
+
+compras.push(`$${encontrado.precio}`);
+
+let seguir = prompt("Desea agrera otra prenda?");
+while (seguir === "si") {
+    eleccion = prompt("Ingresa el nombre producto seleccionado: ");
+    let encontradoDos = productos.find((item) => item.nombre === eleccion);
+
+    compras.push(`$${encontradoDos.precio}`);
+
+    seguir = prompt("Desea agrera otra prenda?");
 }
 
-function mostrarTotal() {
-    alert("Subtotal $" + total);
+if (seguir === "no") {
+    compras.forEach((comprasFinal) => {
+        alert(`total: $ ${compras.precio}`);
+    });
 }
-
-ingresarValor();
-mostrarTotal();
-
-despedida = prompt("desea agregar algun producto mas?");
-if (despedida === "no") {
-    alert("gracias por su compra");
-}else{
-    alert("que mas quiere agregar")
-}
-
